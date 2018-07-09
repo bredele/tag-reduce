266d111ee94a89d6b97f7466e4c873c2b6de69cb
bredele <olivier.wietrich@gmail.com>
2018-07-09 12:00:38 -0600

    Add code documentation

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
-module.exports = ([literals, ...data], map =  arg => arg) => {
+/**
+ * Reduce template literals to a single value
+ * with optional mapping callback.
+ *
+ * @param {Array} template literals
+ * @param {Function?} map
+ * @api public
+ */
+
+module.exports = ([literals, ...data], map = arg => arg) => {
   return literals.reduce((accumulator, value, idx) => {
-    return accumulator +  map(data[idx - 1]) + value
+    return accumulator + map(data[idx - 1]) + value
   })
 }
